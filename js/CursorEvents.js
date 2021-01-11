fb88075289307c710ade49fa75e188f90020fef2
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-01-12 01:59:54 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,79 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" }
+  },
+  init: function() {
+    this.handleClickEvents();
+    this.handleMouseCenterEvents();
+    this.handleMouseLeaveEvents();
+  },
+  handleClickEvents: function() {
+    //  Click Events
+    this.el.addEventListener("click", evt => {
+      const placesContainer = document.querySelector("#places-container");
+      const { state } = placesContainer.getAttribute("tour");
+
+      if (state === "places-list") {
+        const id = this.el.getAttribute("id");
+        const placesId = [
+          "taj-mahal",
+          "budapest",
+          "new-york-city",
+          "eiffel-tower"
+        ];
+        if (placesId.includes(id)) {
+          placesContainer.setAttribute("tour", {
+            state: "view",
+            selectedCard: id
+          });
+        }
+      }
+      
+    });
+  },
+  handleMouseCenterEvents: function() {
+    // Mouse Center Events
+    this.el.addEventListener("mouseenter", () => {
+      const placeContainer = document.querySelector("#places-container");
+      const { state } = placeContainer.getAttribute("tour");
+      if (state === "places-list") {
+        this.handlePlacesListState();
+      }
+    });
+  },
+  handlePlacesListState: function() {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id
+      });
+      this.el.setAttribute("material", {
+        color:"#D76B30",
+        opacity: 1
+      });
+    }
+  },
+  handleMouseLeaveEvents: function() {
+    // Mouse Leave Events
+    this.el.addEventListener("mouseleave", () => {
+      const placesContainer = document.querySelector("#places-container");
+      const { state } = placesContainer.getAttribute("tour");
+      if (state === "places-list") {
+        const { selectedItemId } = this.data;
+        if (selectedItemId) {
+          const el = document.querySelector(`#${selectedItemId}`);
+          const id = el.getAttribute("id");
+          if (id == selectedItemId) {
+            el.setAttribute("material", {
+              color: "#0077CC",
+              opacity: 1
+            });
+          }
+        }
+      }
+    });
+  },
+  
+});
